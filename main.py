850f504d7385dfaec5759dd1f375cbd5cb585dd0
yh-nr <ynaru911@gmail.com>
2023-09-01 00:31:01 +0900

    pdf image reader

--- main.py
+++ main.py
@@ -1,102 +1,64 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
+import pypdf
 
-# タイトルとテキストを記入
-# タイトルとテキストを記入
-st.title('Streamlit 基礎')
-st.write('Hello World!')
+markdown = '''
+# PDFファイルから画像抽出
+### 説明
+* PDFファイルから画像を抽出して表示します。
 
-# データフレームの準備
-df = pd.DataFrame({
-    '1列目': [1,2,3,4],
-    '2列目': [10,20,30,40]
-})
+'''
 
-# 動的なテーブル
-st.dataframe(df)
+st.markdown(markdown)
 
-# 引数を私用した動的テーブル
-st.dataframe(df.style.highlight_max(axis =0), width = 100, height= 150)
-
-# 静的テーブル
-st.table(df)
-
-# 10 行 3 列のデータフレームを準備
-df = pd.DataFrame(
-    np.random.rand(10,3),
-    columns = ['a', 'b', 'c']
-)
-
-# 折れ線グラフ
-st.line_chart(df)
-
-# 面グラフ
-st.area_chart(df)
-
-# 棒グラフ
-st.bar_chart(df)
-
-# プロットする乱数をデータフレームで用意
-df = pd.DataFrame(
-
-    # 乱数 + 新宿の緯度と経度
-    np.random.rand(100,2) / [50, 50] + [35.69, 139.70],
-    columns = ['lat', 'lon']
-)
-
-# マップをプロット
-st.map(df)
-
-# Pillow
-from PIL import Image
-
-# 画像の読み込み
-img = Image.open('iris.jpg')
-st.image(img,caption = 'Iris', use_column_width =True)
+pdf_file = st.file_uploader("ファイルをアップロード", type='pdf')
+# PDFファイルのメタデータを取得する
+# pdf_info = pdf_reader.metadata
 
 
-# チェックボックス
-if st.checkbox('Show Image'):
-    img = Image.open('iris.jpg')
-    st.image(img,caption = 'Iris' , use_column_width = True)
 
-# セレクトボックス
-option = st.selectbox(
-    '好きな数字を入力してください。',
-    list(range(1, 11))
-)
+if pdf_file is not None:
 
-'あなたの好きな数字は' , option , 'です。'
+    pdf_reader = pypdf.PdfReader(pdf_file) 
+    image_list = []
+    for page in pdf_reader.pages:
+        for image_file_object in page.images:
+            image_list.append(image_file_object)
 
+    if len(image_list):
+        # セッションの状態を初期化
+        if 'slider_value' not in st.session_state:
+            st.session_state.slider_value = 0
 
-# テキスト入力による値の動的変更
-text = st.sidebar.text_input('あなたの好きなスポーツを教えて下さい。')
-'あなたの好きなスポーツ：' , text
-
-# スライダーによる値の動的変更
-condition = st.sidebar.slider('あなたの今の調子は？', 0, 100, 50)
-'コンディション：' , condition
+        # 列を作成
+        col1, col2, col3 = st.columns(3)
 
 
-# expander
-expander1 = st.expander('質問1')
-expander1.write('質問1の回答')
-expander2 = st.expander('質問2')
-expander2.write('質問2の回答')
-expander3 = st.expander('質問3')
-expander3.write('質問3の回答')
+        value = st.session_state.get("slider_value", 0)
 
 
-import time
+        # ボタンを作成
+        with col1:
+            if st.button("Decrease"):
+                value -= 1
+                value = max(value, 0)  # 最小値は0に設定
 
-latest_iteration = st.empty()
-bar = st.progress(0)
+        with col3:
+            if st.button("Increase"):
+                value += 1
+                value = min(value, len(image_list)-1)  # 最大値は100に設定
 
-# プログレスバーを0.1秒毎に進める
-for i in range(100):
-    latest_iteration.text(f'Iteration{i + 1}')
-    bar.progress(i + 1)
-    time.sleep(10)
+        with col2:
+            slider=st.slider("指定範囲", 0, len(image_list)-1, value)
 
-'Done'
+        st.text(image_list[slider].name)
+        st.image(image_list[slider].image)
+
+        # スライダーの値をsession_stateに保存
+        st.session_state.slider_value = value
+    
+    else:
+        st.warning("PDFファイルに画像がありません")
+
+else:
+    st.warning("PDFファイルをアップロードして下さい")
+
