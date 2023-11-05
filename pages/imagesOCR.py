f5b8d65adeba29aee2dbd126ef5cec8c302ce865
yh-nr <ynaru911@gmail.com>
2023-11-05 17:18:42 +0900

    image2OCR

--- pages/imagesOCR.py
+++ pages/imagesOCR.py
@@ -10,67 +10,35 @@ with open("docs/PDFimagesOCR.md", "r") as file:
 st.set_page_config(page_title='PDF内画像OCR')
 st.markdown(content)
 
-pdf_file = st.file_uploader("ファイルをアップロード", type='pdf')
+img_file = st.file_uploader("ファイルをアップロード", type='jpg')
 
 # 新しいファイルがアップロードされたかどうかの状態を持つ
 if 'last_uploaded_file' not in st.session_state:
     st.session_state.last_uploaded_file = None
 
 # 新しいPDFファイルがアップロードされたか確認
-if st.session_state.last_uploaded_file != pdf_file:
+if st.session_state.last_uploaded_file != img_file:
     st.session_state.slider_value = 0
-    st.session_state.last_uploaded_file = pdf_file
+    st.session_state.last_uploaded_file = img_file
 
+if img_file is not None:
 
+    if 'slider_value' not in st.session_state:
+        st.session_state.slider_value = 0
 
-if pdf_file is not None:
+    lang = st.radio("OCR言語", ("jpn", "eng"), horizontal=True)
 
-    pdf_reader = pypdf.PdfReader(pdf_file) 
-    image_list = []
+    slider=st.slider("指定範囲", 0, len(image_list)-1, value)
 
-    for page in pdf_reader.pages:
-        for image_file_object in page.images:
-            image_list.append(image_file_object)
+    st.image(img_file)
 
-    if len(image_list):
-        # セッションの状態を初期化
+    # スライダーの値をsession_stateに保存
+    value = st.session_state.get("slider_value", 0)
+    st.session_state.slider_value = value
 
-        st.markdown(f'### このPDFには{len(image_list)}個の画像があります。')
-
-        if 'slider_value' not in st.session_state:
-            st.session_state.slider_value = 0
-
-        # 列を作成
-        col1, col2, col3 = st.columns([3,1,1])
-        value = st.session_state.get("slider_value", 0)
-
-        lang = st.radio("OCR言語", ("jpn", "eng"), horizontal=True)
-
-        # ボタンを作成
-        with col2:
-            if st.button("Prev"):
-                value -= 1
-                value = max(value, 0)  # 最小値は0に設定
-
-        with col3:
-            if st.button("Next"):
-                value += 1
-                value = min(value, len(image_list)-1)  # 最大値は100に設定
-
-        with col1:
-            slider=st.slider("指定範囲", 0, len(image_list)-1, value)
-
-        st.text(image_list[slider].name)
-        st.image(image_list[slider].image)
-
-        # スライダーの値をsession_stateに保存
-        st.session_state.slider_value = value
-
-        # txt = st.code('OCR結果',value=pytesseract.image_to_string(image_list[slider].image, lang=lang))
-        txt = st.code(pytesseract.image_to_string(image_list[slider].image, lang=lang))
+    # txt = st.code('OCR結果',value=pytesseract.image_to_string(image_list[slider].image, lang=lang))
+    txt = st.code(pytesseract.image_to_string(img_file, lang=lang))
     
-    else:
-        st.warning("PDFファイルに画像がありません")
 
 else:
     st.warning("PDFファイルをアップロードして下さい")
